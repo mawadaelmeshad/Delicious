d46ab77893781a694b9d05ce75533c57f22a5410
mawadaelmeshad <mawadahassan21@gmail.com>
2024-09-13 14:01:28 +0300

    pagination logic is done

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -1,28 +1,41 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBPagination, MDBPaginationItem, MDBPaginationLink } from 'mdb-react-ui-kit';
 
-function Pagination() {
+function Pagination({ itemsPerPage, currentPage, totalItems, setCurrentPage }) {
+    const pageNumbers = [...Array(Math.ceil(totalItems / itemsPerPage)).keys()];
+
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex + 1); 
+    };
+
     return (
-            <nav className='flex justify-center font-bold my-2'>
-            <MDBPagination className='mb-0'>
-                <MDBPaginationItem>
-                <MDBPaginationLink href='#'>Previous</MDBPaginationLink>
+        <nav className='flex justify-center font-bold my-2'>
+            <MDBPagination className='mb-0 cursor-pointer'>
+                <MDBPaginationItem
+                    disabled={currentPage === 1}
+                    onClick={() => handlePageChange(currentPage - 2)}
+                >
+                    <MDBPaginationLink previous />
                 </MDBPaginationItem>
-                <MDBPaginationItem>
-                <MDBPaginationLink href='#'>1</MDBPaginationLink>
-                </MDBPaginationItem>
-                <MDBPaginationItem>
-                <MDBPaginationLink href='#'>2</MDBPaginationLink>
-                </MDBPaginationItem>
-                <MDBPaginationItem>
-                <MDBPaginationLink href='#'>3</MDBPaginationLink>
-                </MDBPaginationItem>
-                <MDBPaginationItem>
-                <MDBPaginationLink href='#'>Next</MDBPaginationLink>
+                {pageNumbers.map((number) => (
+                    <MDBPaginationItem
+                        key={number}
+                        active={currentPage === number + 1}
+                        onClick={() => handlePageChange(number)}
+                    >
+                        <MDBPaginationLink>{number + 1}</MDBPaginationLink>
+                    </MDBPaginationItem>
+                ))}
+                <MDBPaginationItem
+                    disabled={currentPage === pageNumbers.length}
+                    onClick={() => handlePageChange(currentPage)}
+                >
+                    <MDBPaginationLink next />
                 </MDBPaginationItem>
             </MDBPagination>
-            </nav>
+        </nav>
     );
 }
+
 export default Pagination;
--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -2,10 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/home.css';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle, MDBInputGroup } from 'mdb-react-ui-kit';
+import Pagination from '../components/Pagination';
 import Loader from '../components/Loader';
 
 function Home() {
     const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 6;
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetch('https://dummyjson.com/recipes')
@@ -18,13 +22,17 @@ function Home() {
             });
     }, []); 
 
-    const [searchTerm, setSearchTerm] = useState('');
 
-    const filteredRecipes = data.filter(r =>
+    // Determine current items to display based on page
+    const indexOfLastRecipe = currentPage * itemsPerPage;
+    const indexOfFirstRecipe = indexOfLastRecipe - itemsPerPage;
+    const currentRecipes = data.slice(indexOfFirstRecipe, indexOfLastRecipe);
+    const filteredRecipes = currentRecipes.filter(r =>
         r.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    if(filteredRecipes.length==0){
-        return <Loader />
+
+    if (currentRecipes.length === 0) {
+        return <Loader />;
     }
 
     return (
@@ -35,23 +43,29 @@ function Home() {
             </MDBInputGroup>
             <div className="card-container"> 
                 {filteredRecipes.map((recipe) => (
-                        <MDBCard key={recipe.id} style={{width:'20rem'}}>
-                            <MDBCardImage 
-                                src={recipe.image} 
-                                position='center' 
-                                alt='Card Image' 
-                                style={{width:'400px'}}
-                            />
-                            <MDBCardBody>
-                                <MDBCardTitle className='text-black font-bold'>{recipe.name}</MDBCardTitle>
-                                <MDBCardText>
-                                    {recipe.instructions.slice(0,recipe.instructions.length/1.5)}
-                                </MDBCardText>
-                                <MDBBtn href={`/recipe/${recipe.id}`} style={{backgroundColor:'#4FA512'}}>Read more ...</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                        ))}
+                    <MDBCard key={recipe.id} style={{width:'20rem'}}>
+                        <MDBCardImage 
+                            src={recipe.image} 
+                            position='center' 
+                            alt='Card Image' 
+                            style={{width:'400px'}}
+                        />
+                        <MDBCardBody>
+                            <MDBCardTitle className='text-black font-bold'>{recipe.name}</MDBCardTitle>
+                            <MDBCardText>
+                                {recipe.instructions.slice(0, recipe.instructions.length / 1.5).join(' ')}
+                            </MDBCardText>
+                            <MDBBtn href={`/recipe/${recipe.id}`} style={{backgroundColor:'#4FA512'}}>Read more ...</MDBBtn>
+                        </MDBCardBody>
+                    </MDBCard>
+                ))}
             </div>
+            <Pagination
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                totalItems={data.length}
+                setCurrentPage={setCurrentPage}
+            />
         </div>
     );
 }
